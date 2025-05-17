4ec0fae4791a629a5ec838a2ebbe2b3148a14deb
Alba Perez <albaperezbaldo@gmail.com>
2025-05-17 16:48:34 +0200

    lab done

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <nav className="nav-section">
+        <img className="logo" src='/discord-logo-white.png' />
+        <img className="menu" src='/menu-icon.png' />
+      </nav>
+    <div className='body'>
+    <h1 className='title'>IMAGINE A PLACE...</h1>
+    <p className='text'>...where you can belong to a school club, a gaming group, or a worldwide art community. Where just you and a handful of friends can spend time together. A place that makes it easy to talk every day and hang out more often.</p>
+    <button className='button-one'>Download for Mac</button>
+    <button className='button-two'>Open Discord in your browser</button>
+    <img className="background" src='/discord-background.png' />
+
+    </div>
+    </div>
+  )
+}
+
+export default App
